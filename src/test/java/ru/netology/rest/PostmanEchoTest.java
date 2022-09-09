46d3c2974e022f539a32a1fb2a9d3201bf062cd0
Tatyana Dmitrieva <visyar76@gmail.com>
2022-09-09 12:26:42 +0300

    Fixed test

--- src/test/java/ru/netology/rest/PostmanEchoTest.java
+++ src/test/java/ru/netology/rest/PostmanEchoTest.java
@@ -13,14 +13,14 @@ public class PostmanEchoTest {
         given()
                 .baseUri("https://postman-echo.com")
                 .contentType("text/plain; charset=UTF-8")
-                .body("some data") // отправляемые данные (заголовки и query можно выставлять аналогично)
+                .body("some value") // отправляемые данные (заголовки и query можно выставлять аналогично)
 // Выполняемые действия
                 .when()
                 .post("/post")
 // Проверки
                 .then()
                 .statusCode(200)
-                .body("data",equalTo("s data"));
+                .body("data",equalTo("some value"));
         ;
     }
 }
